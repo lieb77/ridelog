28fc20f1a73fe4a8c94f4b0cf0e555369ac50673
Paul Lieberman <lieb77@gmail.com>
2023-12-24 19:25:59 +0000

    Moved code from form to controller

---
+++ src/Controller/RidelogController.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+/**
+ * @file
+ *
+ * Provides a controller class for the ridelog module
+ *
+ */
+ 
+namespace Drupal\ridelog\Controller;
+
+use Drupal\ridelog\RideLog;
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for ridelog routes.
+ */
+final class RidelogController extends ControllerBase {
+
+    /**
+     * Builds the response.
+     */
+	public function __invoke(): array {
+
+		$ridelog = new RideLog();
+    
+    	$output = "<table>";
+    	$rides = $ridelog->get_rides();
+		foreach ($rides as $ride) {
+			$output .= "<tr>";
+			foreach($ride as $field) {
+				$output .= "<td>" . $field . "</td>"; 
+			}
+			$toutput .= "</tr>";
+		}
+
+    	$output .= "</table>";
+    	
+    	$build['content'] = [
+      		'#type' => 'item',
+      		'#markup' => $this->t($output)),
+    	];
+
+    	return $build;
+	}
+
+}
