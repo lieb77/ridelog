e82b16ec8122993eb51c5f487659043f267b7dcd
Paul Lieberman <lieb@paullieberman.org>
2025-12-16 15:20:21 -0800

    Adding Single Directory Components

--- src/Controller/RidelogController.php
+++ src/Controller/RidelogController.php
@@ -54,37 +54,40 @@ final class RidelogController extends ControllerBase {
 
 	public function yearly() {
 	
-	  // Get query string
-	  $request = \Drupal::request();
-    $query   = $request->query;
-    $year    = $query->get('year');
-    $bike    = $query->get('bike');
-
-    $filter = [];
-    if ($year) {
-      $filter['year'] = $year;
-    }
-
-    if ($bike) {
-      $filter['bike'] = $bike;
-    }
-
+		// Get query string
+		$request = \Drupal::request();
+		$query   = $request->query;
+		$year    = $query->get('year');
+		$bike    = $query->get('bike');
+		
+		$filter = [];
+		if ($year) {
+		  $filter['year'] = $year;
+		}
+		
+		if ($bike) {
+		  $filter['bike'] = $bike;
+		}
+		
 		$ridelog = new RideLog();
-    $data = $ridelog->yearly_totals($filter);
-
-	$render_array = [  
-      '#theme'       => 'yeartotals',
-      '#bikes'	     => $data['bikes'],
-      '#rides'       => $data['rides'], 
-      '#year_total'  => $data['year_total'],
-      '#month_total' => $data['month_total'], 
-      '#bike_total'  => $data['bike_total'],
-      '#stats'	     => $data['stats'],
-      '#grand'       => $data['grand'],
-    ];
-        
-    return $render_array;
-    	
+		$data = $ridelog->yearly_totals($filter);
+		
+		$props = [  
+			'bikes'	      => $data['bikes'],
+			'rides'       => $data['rides'], 
+			'year_total'  => $data['year_total'],
+			'month_total' => $data['month_total'], 
+			'bike_total'  => $data['bike_total'],
+			'stats'	   	  => $data['stats'],
+			'grand'       => $data['grand'],
+		];
+		
+		return [
+      		'#type' 	 => 'component',
+      		'#component' => 'ridelog:yeartotals',
+      		'#props' 	 => $props,
+    	];	
 	}
-
+	
+// End-of-class	
 }
