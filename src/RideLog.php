e3ca18352036ac8c2c0d32813f64f4ae6e9cee89
Paul Lieberman <lieb77@gmail.com>
2025-12-18 10:02:55 -0800

    Updating to use dependency injection

--- src/RideLog.php
+++ src/RideLog.php
@@ -13,9 +13,11 @@
 
 namespace Drupal\ridelog;
 
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
 use Drupal\ridelog\Ride;
 use Drupal\ridelog\Rides;
-use Drupal\node\Entity\Node;
 
 class RideLog {
 
@@ -23,50 +25,49 @@ class RideLog {
 	protected $rides;
 	protected $bikes;
 	protected $fulldata;
-
-  	protected $logger;
   	protected $storage;
   	protected $query;
 
 	// constructor initializes database query
-	public function __construct($params = []) {
-	
-		$this->logger = \Drupal::logger('ridelog');
-      	$this->storage = \Drupal::entityTypeManager()->getStorage('node');
-      	
+	public function __construct(
+        protected EntityTypeManagerInterface $entityTypeManager,
+        protected LoggerChannelInterface $logger) {
+
+      	$this->storage = $entityTypeManager->getStorage('node');
+
       	$this->rideclass = new Rides();
-		
+
 		// Get the array of bikes
 		$this->query_bikes();
-		
-		// query rides for each year    
-    	$this->query_rides("2003"); 
-    	
+
+		// query rides for each year
+    	$this->query_rides("2003");
+
     	$this->monthly_summary();
-    		
-		$this->logger->notice('Constructor finished');		
-	}	
+
+		$this->logger->notice('Constructor finished');
+	}
 
 	/**
-	 * return an array of rides	
+	 * return an array of rides
 	 *
-	 */	
-	public function get_rides() {			
+	 */
+	public function get_rides() {
 		return $this->rides;
   	}
-	
+
 		public function monthly_summary() {
 			$months = ['Jan','Feb','Mar','Apr','May','Jun','Jul', 'Aug','Sep','Oct','Nov','Dec'];
 			$year = date('Y');
-			
+
 			// Loop through the months
 			foreach ($months as $month){
-				$stats[$month] = $this->rideclass->stats_by_month_year($month, $year);			
+				$stats[$month] = $this->rideclass->stats_by_month_year($month, $year);
 			}
 			return $stats;
 		}
-	
-	
+
+
 	public function yearly_totals($filter) {
 		$months = ['Jan','Feb','Mar','Apr','May','Jun','Jul', 'Aug','Sep','Oct','Nov','Dec'];
 
@@ -83,7 +84,7 @@ class RideLog {
   		  $minyear = 2004;
     		$maxyear = date('Y');
     	}
-  		
+
   		// Loop though the years
   		for ($year = $maxyear; $year >= $minyear; $year--) {
   			$year_total[$year] = 0;
@@ -91,35 +92,35 @@ class RideLog {
 
   			// Get the yearly summaries
 			  $stats[$year] = $this->rideclass->stats_by_year($year);
-  			
+
   			// Must initialize each bike total for the year
   			foreach ($this->bikes as $nid => $bike){
   					$bike_total[$year][$bike] = 0;
   			}
-  			
+
   			// Loop through the months
   			foreach ($months as $month){
   				$month_total[$year][$month]  = 0;
-  				
+
   				// Loop through the bikes
   				foreach ($this->bikes as $nid => $bike){
   					$rides[$year][$month][$bike] = 0;
-  					$full = $this->rideclass->rides_by_bike_year_month($bike, $year, $month);  				  				
-  					
+  					$full = $this->rideclass->rides_by_bike_year_month($bike, $year, $month);
+
   					// Loop through the rides
   					foreach ($full as $ride) {
-  						$miles = $ride->get_miles(); 
+  						$miles = $ride->get_miles();
   						$rides[$year][$month][$bike] += $miles;
   						$month_total[$year][$month] += $miles;
   						$year_total[$year]   += $miles;
                         $bike_total[$year][$bike]   += $miles;
                         $grand['miles'] += $miles;
                         $grand['rides']++;
-  					}					
+  					}
   				}
   			}
 		}
-		
+
 		// build an array of bikes with miles for each year
 		// unset bikes with no miles in each array
 		foreach ($this->bikes as $nid => $bike){
@@ -134,26 +135,26 @@ class RideLog {
 					}
 				}
 			}
-        }				
+        }
 
         // Grand totals
 		$grand['years'] = $numyears;
-	    $grand['avg'] = floor($grand['miles'] / $numyears);	
-		
+	    $grand['avg'] = floor($grand['miles'] / $numyears);
+
 		return [
 			'bikes'		  => $bikes,
-			'rides'       => $rides, 
+			'rides'       => $rides,
 			'year_total'  => $year_total,
 			'month_total' => $month_total,
 			'bike_total'  => $bike_total,
             'stats'		  => $stats,
             'grand'       => $grand,
-		];	
+		];
 	}
-		
-	protected function calculate() {	
+
+	protected function calculate() {
 		$months = ['Jan','Feb','Mar','Apr','May','Jun','Jul', 'Aug','Sep','Oct','Nov','Dec'];
-		
+
 		// Loop through years - log starts in 2004
   		// ----------------------------------------
   		$yr = date('Y');
@@ -169,35 +170,35 @@ class RideLog {
 			$total       = 0;   // Total number of miles
 			$numrides    = 0;   // Number of rides
 
-			// initialize each bike 
+			// initialize each bike
 			foreach ($this->bikes as $nid => $bike){
-				$bike_total[$bike] = 0; 
+				$bike_total[$bike] = 0;
 			}
-			
-			// initialize each month 
+
+			// initialize each month
 			foreach ($months as $mon) {
-				$month_total[$mon] = 0;		
-				foreach ($this->bikes as $nid => $bike){	
+				$month_total[$mon] = 0;
+				foreach ($this->bikes as $nid => $bike){
 					$month_bike[$mon][$bike] = 0;
 				}
-			}			  		    		
-    			
+			}
+
     		foreach ($this->rides as $nid => $ride) {
     			$bike  = $ride['field_bike'];
     			$miles = $ride['field_miles'];
     			$date  = $ride['field_ridedate'];
-    			    			    		
+
     			$mon = date('M', strtotime($date));
-    		 
+
 				// Build some arrays to hold the data
 				// ---------------------------------------
-				$month_total[$mon] += $miles; 
+				$month_total[$mon] += $miles;
 			    $bike_total[$bike] += $miles;
-				$month_bike[$mon][$bike] += $miles; 				
+				$month_bike[$mon][$bike] += $miles;
 				$total += $miles;
-				$numrides++;	
-			}	
-					
+				$numrides++;
+			}
+
 			// Remove bikes with zero miles for this year
 			foreach ($this->bikes as $nid => $bike) {
 			    if ($bike_total[$bike] < 1) {
@@ -207,31 +208,31 @@ class RideLog {
 			// Remove bikes with zero miles for eqch month
 			foreach ($months as $mon) {
 				foreach ($this->bikes as $nid => $bike) {
-					if ($month_bike[$mon][$bike] < 1) {		
+					if ($month_bike[$mon][$bike] < 1) {
 						unset($month_bike[$mon][$bike]);
 					}
 				}
 			}
-			
+
 			// Save data for this year
     		$this->fulldata[$year] = [
-    			$total, $numrides, $month_total, $bike_total, $month_bike    		
+    			$total, $numrides, $month_total, $bike_total, $month_bike
     		];
-    		
-			break;    		
-    	}	
+
+			break;
+    	}
 	}
-	
-	
+
+
 
 	/**
-	 * Get an array of rides	
+	 * Get an array of rides
 	 *
-	 */	
-		
+	 */
+
 	protected function query_rides($year) {
-	
-		// Will return everything from $year on		
+
+		// Will return everything from $year on
    		$nids = \Drupal::entityQuery('node')
    	  		->accessCheck(TRUE)
       		->condition('type', 'ride')
@@ -239,41 +240,41 @@ class RideLog {
 //      		->condition('field_bike.entity:node.title', $bike)
       		->sort('field_ridedate', 'DESC')
       		->execute();
-		
+
 		$nodes = $this->storage->loadMultiple($nids);
-		
+
 		$rides = [];
-		// loop through the results 
+		// loop through the results
 		foreach ($nodes as $nid => $ride) {
-	     		
+
       		// Get the title (route)
       		$route = $ride->getTitle();
-      		
+
       		// Get the bike
-      		$value = $ride->get('field_bike')->getValue();      		
+      		$value = $ride->get('field_bike')->getValue();
       		$bike_nid = $value[0]['target_id'];
-      		$bike = $this->bikes[$bike_nid];      		      		
-      		
+      		$bike = $this->bikes[$bike_nid];
+
       		// Get the miles
       		$value = $ride->get('field_miles')->getValue();
         	$miles = $value[0]['value'];
-      		
+
       		// Get the date
 			$value = $ride->get('field_ridedate')->getValue();
-			$date = $value[0]['value'];			
-			
+			$date = $value[0]['value'];
+
 			$this->rides[$nid]['title']          = $route;
 			$this->rides[$nid]['field_bike']     = $bike;
 			$this->rides[$nid]['field_miles']    = $miles;
 			$this->rides[$nid]['field_ridedate'] = $date;
-			
+
 			$year  = date('Y', strtotime($date));
 			$month = date("M", strtotime($date));
-			
+
 			// Save the data
 			$this->rideclass->add_ride($nid, $year, $month, $bike, $miles);
 
-    	}   
+    	}
 	}
 
 	/**
@@ -288,13 +289,13 @@ class RideLog {
    	  		->accessCheck(TRUE)
       		->condition('type', 'bicycle')
       //		->condition('field_bike_activre', TRUE)
-      		->execute();		
+      		->execute();
 		$nodes = $this->storage->loadMultiple($nids);
-		// loop through the results 
+		// loop through the results
     	foreach ($nodes as $nid => $bike) {
 	  		$title = $bike->getTitle();
     		$this->bikes[$nid] = $title;
-    	}    
+    	}
 	}
 
 // End of class
